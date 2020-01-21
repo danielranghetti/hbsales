cba24a171b0d5c76a545493a061a61667861fafa
daniel.ranghetti <danielranghetti48@gmail.com>
2020-01-21 08:33:22 -0300

    implementado validacao de cnpj

--- src/main/java/br/com/hbsis/fornecedor/FornecedorService.java
+++ src/main/java/br/com/hbsis/fornecedor/FornecedorService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,6 +46,7 @@ import java.util.Optional;
     public FornecedorDTO save(FornecedorDTO fornecedorDTO) {
 
         this.validate(fornecedorDTO);
+
 
         LOGGER.info("Salvando fornecedor");
         LOGGER.debug("Fornecedor: {}", fornecedorDTO);
@@ -99,6 +101,9 @@ import java.util.Optional;
         if (StringUtils.isEmpty(fornecedorDTO.geteMail())) {
             throw new IllegalArgumentException("E-mail não deve ser nulo/vazio");
         }
+        if (!isCNPJ(fornecedorDTO.getCnpj())){
+            throw new IllegalArgumentException("Cnpj invalido");
+        }
     }
 
     public FornecedorDTO update(FornecedorDTO fornecedorDTO, Long id) {
@@ -138,6 +143,59 @@ import java.util.Optional;
         LOGGER.info("Executando delete para fornecedor de ID: [{}]", id);
 
         this.conexaoFornecedor.deletePorId(id);
+    }
+    public static boolean isCNPJ(String CNPJ) {
+        if (CNPJ.equals("00000000000000") || CNPJ.equals("11111111111111") ||
+                CNPJ.equals("22222222222222") || CNPJ.equals("33333333333333") ||
+                CNPJ.equals("44444444444444") || CNPJ.equals("55555555555555") ||
+                CNPJ.equals("66666666666666") || CNPJ.equals("77777777777777") ||
+                CNPJ.equals("88888888888888") || CNPJ.equals("99999999999999") ||
+                (CNPJ.length() != 14))
+            return(false);
+
+        char dig13, dig14;
+        int sm, i, r, num, peso;
+
+
+        try {
+
+            sm = 0;
+            peso = 2;
+            for (i=11; i>=0; i--) {
+                num = (int)(CNPJ.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso + 1;
+                if (peso == 10)
+                    peso = 2;
+            }
+
+            r = sm % 11;
+            if ((r == 0) || (r == 1))
+                dig13 = '0';
+            else dig13 = (char)((11-r) + 48);
+
+            sm = 0;
+            peso = 2;
+            for (i=12; i>=0; i--) {
+                num = (int)(CNPJ.charAt(i)- 48);
+                sm = sm + (num * peso);
+                peso = peso + 1;
+                if (peso == 10)
+                    peso = 2;
+            }
+
+            r = sm % 11;
+            if ((r == 0) || (r == 1))
+                dig14 = '0';
+            else dig14 = (char)((11-r) + 48);
+
+
+            if ((dig13 == CNPJ.charAt(12)) && (dig14 == CNPJ.charAt(13)))
+                return(true);
+            else return(false);
+        } catch (InputMismatchException erro) {
+            return(false);
+        }
     }
 
 
